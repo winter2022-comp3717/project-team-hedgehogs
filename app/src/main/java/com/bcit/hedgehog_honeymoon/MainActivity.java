3e4f922bd0fe3af9a21c0a9d65c91fa92542f348
Samuel Cheon <samuelcalling@gmail.com>
2022-03-06 21:55:30 -0800

    - created an image fragment (of hedgehog) for main Activity - enabled 'play' and 'setting' buttons functionality - adding a little stuff to the fragment_settings.xml - set GameActivity.xml to be default 'landscape' orientation when running app

--- app/src/main/java/com/bcit/hedgehog_honeymoon/MainActivity.java
+++ app/src/main/java/com/bcit/hedgehog_honeymoon/MainActivity.java
@@ -1,8 +1,11 @@
 package com.bcit.hedgehog_honeymoon;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentTransaction fragmentTransaction;
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainerView_main, new ImageHedgehogFragment());
+        fragmentTransaction.commit();
+
     }
+
+    public void goToGameActivity(View v) {
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
+    public void openSettings(View v) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainerView_main, new SettingsFragment());
+        fragmentTransaction.commit();
+    }
+
 }
