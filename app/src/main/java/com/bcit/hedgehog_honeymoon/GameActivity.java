4a6216bad83127dc0e8bbcab2dc43759b25a3b4a
Nosewall <nolannordwall@gmail.com>
2022-03-21 13:37:40 -0700

    Merged Samuel to main

--- app/src/main/java/com/bcit/hedgehog_honeymoon/GameActivity.java
+++ app/src/main/java/com/bcit/hedgehog_honeymoon/GameActivity.java
@@ -11,4 +11,8 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
     }
+
+    public void incrementHedgeHog(){
+
+    }
 }
