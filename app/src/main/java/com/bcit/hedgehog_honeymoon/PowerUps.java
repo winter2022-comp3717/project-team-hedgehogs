f47a2c8612d76b25c1796ddd0aa9fd0eec2ae9e9
Kat Sukkau <Katsukkau@gmail.com>
2022-03-25 11:02:08 -0700

    created power up class and recycler

---
+++ app/src/main/java/com/bcit/hedgehog_honeymoon/PowerUps.java
@@ -0,0 +1,37 @@
+package com.bcit.hedgehog_honeymoon;
+
+public class PowerUps {
+    String name;
+    String description;
+    int imageId;
+
+    public PowerUps(String name, String description, int imageId) {
+        this.name = name;
+        this.description = description;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+}
